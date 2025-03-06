f8e5b96ea93dc308ebc2d426ab8625a1aa9e2ff1
POOH <43782789+PO0OH@users.noreply.github.com>
2025-03-06 22:06:51 +0900

    [미션1] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/limc/storevibe/StorevibeApplication.kt
@@ -0,0 +1,11 @@
+package com.limc.storevibe
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class StorevibeApplication
+
+fun main(args: Array<String>) {
+	runApplication<StorevibeApplication>(*args)
+}
---
+++ src/test/kotlin/com/limc/storevibe/StorevibeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.limc.storevibe
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class StorevibeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
